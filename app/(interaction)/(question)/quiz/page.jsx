28f410751b24c8bf9b83658577c80be25ae45299
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-01-24 14:20:39 +0330

    UPDATE: complete quiz

--- app/(interaction)/(question)/quiz/page.jsx
+++ app/(interaction)/(question)/quiz/page.jsx
@@ -2,6 +2,7 @@
 
 import {quiz} from "../../../data";
 import {useState} from "react";
+import {reset} from "next/dist/lib/picocolors";
 
 export default function Quiz() {
 
@@ -9,9 +10,15 @@ export default function Quiz() {
     const [canPassToNextQuestion, setCanPassToNextQuestion] = useState(false)
     const [showQuizResult, setShowQuizResult] = useState(false)
     const [selectedAnswerIndex, setSelectedAnswerIndex] = useState()
+    const [isSelectedAnswerCorrect, setIsSelectedAnswerCorrect] = useState();
+    const [score, setScore] = useState({
+        score: 0,
+        correctAnswers: 0,
+        wrongAnswers: 0,
+    })
 
     const {questions} = quiz
-    console.log(questions)
+    // console.log(questions)
 
     const activeQuestion = questions[activeQuestionIndex]
 
@@ -21,32 +28,80 @@ export default function Quiz() {
         correctAnswer
     } = activeQuestion
 
+    const onAnswerSelected = (answer, index) => {
+        setSelectedAnswerIndex(index)
+        setCanPassToNextQuestion(true)
+        setIsSelectedAnswerCorrect(answer === correctAnswer)
+    }
+
+    const onNextQuestionSelected = () => {
+        // console.log(isSelectedAnswerCorrect)
+        setSelectedAnswerIndex(null)
+        setScore((prevState) => isSelectedAnswerCorrect ?
+            {...prevState, score: prevState.score + 5, correctAnswers: prevState.correctAnswers + 1} :
+            {...prevState, wrongAnswers: prevState.wrongAnswers + 1})
+        // console.log(showQuizResult, score)
+        if (activeQuestionIndex !== questions.length - 1) {
+            // console.log(activeQuestionIndex)
+            setActiveQuestionIndex(prevState => prevState + 1)
+        } else {
+            // console.log(showQuizResult)
+            setActiveQuestionIndex(0)
+            setShowQuizResult(true)
+        }
+        setCanPassToNextQuestion(false)
+    }
+
     return (
         <>
-            {
-                !showQuizResult ? (
-                    <div className='flex-center'>
-                        <h3>{question}</h3>
-                        {
-                            answers.map((answer, index) => (
-                                <button
-                                    key={index}
-                                    onClick={() => {
-                                        setSelectedAnswerIndex(index)
-                                        setCanPassToNextQuestion(true)
-                                    }}
+            <h1>صفحه آزمون</h1>
+            <div>
+                {
+                    !showQuizResult ? (
+                        <h2>آزمون : {activeQuestionIndex + 1}, از {questions.length}</h2>
+                    ) : null
+                }
+            </div>
+            <div>
+                {
+                    !showQuizResult ? (
+                        <div className='flex-center flex-col'>
+                            <h3>{question}</h3>
+                            <div className='flex flex-col mt-4'>
+                                {
+                                    answers.map((answer, index) => (
+                                        <button
+                                            key={index}
+                                            onClick={() => onAnswerSelected(answer, index)}
+                                            className={selectedAnswerIndex === index ? 'bg-red-600' : ''}
+                                        >
+                                            {answer}
+                                        </button>
+                                    ))
+                                }
+                                <button disabled={!canPassToNextQuestion ? 'disabled' : ''}
+                                        onClick={onNextQuestionSelected}
+                                        className={canPassToNextQuestion ? 'cursor-pointer bg-green-700' : 'bg-gray-600'}
                                 >
-                                    {answer}
+                                    {activeQuestionIndex === questions.length - 1 ? 'پایان' : 'بعدی'}
                                 </button>
-                            ))
-                        }
-                    </div>
-                ) : (
-                    <section>
+                            </div>
+                        </div>
+                    ) : (
+                        <section>
+                            <h3>نتایج</h3>
+                            <h4>به طور کلی {(score.score / 25) * 100}% سوالات جواب داده شده است.</h4>
+                            <p>کل سوالات : {questions.length}</p>
+                            <p>سوالات درست : {score.correctAnswers}</p>
+                            <p>سوالات غلط : {score.wrongAnswers}</p>
 
-                    </section>
-                )
-            }
+                            <button onClick={() => window.location.reload()}>
+                                شروع مجدد آزمون
+                            </button>
+                        </section>
+                    )
+                }
+            </div>
         </>
     )
 }
