2612dea5a6db6f9781c2a3da85e0ecc35315482c
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-01-31 15:54:08 +0330

    UPDATE: fix response

--- src/components/item/ProductList.jsx
+++ src/components/item/ProductList.jsx
@@ -1,9 +1,12 @@
-import { setItems } from "../../store/itemSlice";
-import { store } from "../../store";
+import {setItems} from "../../store/itemSlice";
+import {store} from "../../store";
 import ProductCard from "../../components/item/ProductCard";
 
 export default async function ProductList() {
-    const items = await fetch("http:/toprate.test/api/v1/items", { cache: "force-cache" }).then((res) => res.json());
+    const items = await fetch("http:/toprate.test/api/v1/items", {cache: "force-cache"})
+        .then((res) => res.json())
+        .then((response) => response.data);
+
     console.log(items)
     // ✅ Dispatch items to Redux store
     store.dispatch(setItems(items));
@@ -11,9 +14,9 @@ export default async function ProductList() {
     return (
         <div>
             <h2>Products</h2>
-            <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px" }}>
+            <div style={{display: "grid", gridTemplateColumns: "repeat(3, 1fr)", gap: "20px"}}>
                 {items.map((product) => (
-                    <ProductCard key={product.id} product={product} />
+                    <ProductCard key={product.id} product={product}/>
                 ))}
             </div>
         </div>
