13aa7c839eb91d253b0017b1a0e777859da079ac
Mohamadreza Rezaei <me.moham6dreza@gmail.com>
2025-02-15 13:59:17 +0330

    UPDATE: complete middleware.js to redirect, according to lang

--- src/middleware.js
+++ src/middleware.js
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
-import { match } from '@formatjs/intl-localematcher'
+import {NextResponse} from 'next/server'
+import {match} from '@formatjs/intl-localematcher'
 import Negotiator from 'negotiator'
 
 let locales = ['en-US', 'fa-IR', 'fa', 'en']
@@ -8,7 +8,7 @@ let locales = ['en-US', 'fa-IR', 'fa', 'en']
 function getLocale(request) {
     let headers = {}
     request.headers.forEach((value, key) => (headers[key] = value))
-    let languages = new Negotiator({ headers }).languages()
+    let languages = new Negotiator({headers}).languages()
 
     let defaultLocale = 'fa-IR'
 
@@ -16,23 +16,36 @@ function getLocale(request) {
 }
 
 
-
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
+
+    const {pathname} = request.nextUrl
+
+    // localhost:3000/fa/... or localhost:3000/fa
+    const pathnameHasLocale = locales.some(
+        locale => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    )
+
+    if (pathnameHasLocale) return
+
     const locale = getLocale(request)
     console.log(locale)
+    const urlLocale = `${locale}`
+    const urlPath = `${pathname.startsWith('/') ? '' : '/'}${pathname}`
+    return NextResponse.redirect(new URL(urlLocale + urlPath, request.url))
+
     // return NextResponse.redirect(new URL('/maintenance', request.url))
 
     // return NextResponse.rewrite(new URL('/maintenance', request.url))
 
     // conditional statements -> not required any matcher
-    if (request.nextUrl.pathname.startsWith('/about')) { // start with about so /about/a/b/ ...
-        return NextResponse.rewrite(new URL('/about-2', request.url))
-    }
-
-    if (request.nextUrl.pathname.startsWith('/dashboard')) {
-        return NextResponse.rewrite(new URL('/dashboard/user', request.url))
-    }
+    // if (pathname.startsWith('/about')) { // start with about so /about/a/b/ ...
+    //     return NextResponse.rewrite(new URL('/about-2', request.url))
+    // }
+    //
+    // if (pathname.startsWith('/dashboard')) {
+    //     return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+    // }
 }
 
 export const config = {
